2d221bb0512a7847df8970b16e572e39bc245df8
mito-201 <69384480+mito-201@users.noreply.github.com>
2024-02-26 23:34:53 +0900

    add knock87

---
+++ answer/knock87/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int birthday, result = 0;
+    printf("誕生日をYYYYMMDDの形式で入力してください: ");
+    scanf("%d", &birthday);
+
+    printf("運命数は%d\n", result);
+    return 0;
+}
