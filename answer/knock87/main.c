f3320afb7b8dbb3348c1103ed3c83b0577c78ebb
mito-201 <69384480+mito-201@users.noreply.github.com>
2024-02-27 12:00:21 +0900

    dev

--- answer/knock87/main.c
+++ answer/knock87/main.c
@@ -1,11 +1,23 @@
 #include <stdio.h>
 
+
+
 int main(void)
 {
     int birthday, result = 0;
+    
     printf("誕生日をYYYYMMDDの形式で入力してください: ");
     scanf("%d", &birthday);
 
+    // birthdayをintで試す
+    result =+ birthday % 10000000;
+    birthday =- birthday mod 10000000
+    result =+ birthday % 10000000;
+
     printf("運命数は%d\n", result);
+
+    // birthdayをstrで試す
+
+
     return 0;
 }
