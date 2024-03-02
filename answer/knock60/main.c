2ffb09215ceb0992835da9d4ec9c74bdd278ec2b
m_ito <m_ito@casareal.co.jp>
2024-03-03 01:41:28 +0900

    fix

--- answer/knock60/main.c
+++ answer/knock60/main.c
@@ -1,8 +1,31 @@
 #include <stdio.h>
+#include <gtk/gtk.h>
+// gcc `pkg-config --cflags gtk+-3.0` main.c `pkg-config --libs gtk+-3.0`
+// https://docs.gtk.org/gtk3/
 
-int main(void)
+static void
+activate (GtkApplication* app,
+          gpointer        user_data)
 {
-    
+  GtkWidget *window;
 
-    return 0;
+  window = gtk_application_window_new (app);
+  gtk_window_set_title (GTK_WINDOW (window), "Window");
+  gtk_window_set_default_size (GTK_WINDOW (window), 200, 200);
+  gtk_widget_show_all (window);
+}
+
+int
+main (int    argc,
+      char **argv)
+{
+  GtkApplication *app;
+  int status;
+
+  app = gtk_application_new ("org.gtk.example", G_APPLICATION_FLAGS_NONE);
+  g_signal_connect (app, "activate", G_CALLBACK (activate), NULL);
+  status = g_application_run (G_APPLICATION (app), argc, argv);
+  g_object_unref (app);
+
+  return status;
 }
