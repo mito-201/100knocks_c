e40c35db2f74161ad034d8648cc0a50bd11eaf25
mito-201 <69384480+mito-201@users.noreply.github.com>
2024-03-12 22:00:27 +0900

    add knock56

--- answer/knock56/main.c
+++ answer/knock56/main.c
@@ -2,7 +2,27 @@
 
 int main(void)
 {
+    int x;
+    printf("input number: ");
+    scanf("%d", &x);
+
+    int binary_num[16] = {};
+    int i;
     
+    for (i = 0; i < 16; i++) {
+        if( x % 2 == 0) {
+            binary_num[i] = 0;
+        } else {
+            binary_num[i] = 1;
+        }
+        x = x / 2;
+    }
+    
+    for (i = 15; i >= 0; i--) {
+        printf("%d", binary_num[i]);
+    }
+
+    printf("\n");
 
     return 0;
 }
