a002c16525881a22a731376fa2af1b9b4df3af21
mito-201 <69384480+mito-201@users.noreply.github.com>
2024-03-28 17:04:39 +0900

    WIP

--- answer/knock90/main.c
+++ answer/knock90/main.c
@@ -8,19 +8,21 @@
 int card[52];              // シャッフルしたカードデータを格納
 
 int get_random(int min, int max);
+void card_set(void);
 void card_shuffle(void);
 
 int main(void)
 {
 
+    card_set();
     card_shuffle();
 
     // シャッフル後のカードデータ表示
 
     printf("\n\n");
     for(int i = 0; i < 53; i++) {
-        printf("%s\t", card[i]);
-        if (i % 4 == 0) {
+        printf("%d\t", card[i]);
+        if ((i + 1) % 4 == 0) {
             printf("\n");
         } 
     }
@@ -28,41 +30,36 @@ int main(void)
     return 0;
 }
 
-
-void card_shuffle(void)
+void card_set(void)
 {
     int i, j = 1;
 
-    for (i = 0; i < 52; i++){
+    for (i = 0, j = 1; i < 52; i++, j++){
         card[i] = j;
-        j++;
         if ( j > 13) {
-            J = 1;
+            j = 1;
         }
     }
 
+    return;
+}
+
+void card_shuffle(void)
+{
     // 指定した回数分、カードをシャッフルする
     srand((unsigned int)time(NULL));
 
-    int change_card[2];
-    for (i = 0; i < CHANGE_NUM; i++) {
+    int change_card[2], card_temp;
+    for (int i = 0; i < CHANGE_NUM; i++) {
         // 入れ替え元
         change_card[0] = get_random(0, 52);
         // 入れ替え先
         change_card[1] = get_random(0, 52);
 
         // 入れ替え
-        card[ change_card[0] ] = card[ change_card[1][0] ][ change_card[1][1] ];
-        card[ change_card[1] ] = card_data[ change_card[0][0] ][ change_card[0][1] ];
-    }
-
-    // シャッフル後のカードデータ表示
-
-    for(i = 0; i < 13; i++) {
-        printf("%s\t%s\t%s\t%s\n", card[0][i], card[1][i], card[2][i], card[3][i]);
-    }
-    for(i = 0; i < 13; i++) {
-        printf("%s\t%s\t%s\t%s\n", card[0][i], card[1][i], card[2][i], card[3][i]);
+        card_temp = card[ change_card[0] ];
+        card[ change_card[0] ] = card[ change_card[1] ];
+        card[ change_card[1] ] = card_temp;
     }
 
     return;
